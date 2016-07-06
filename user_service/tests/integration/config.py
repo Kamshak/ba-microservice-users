dcee8da8dabbe1c6b6ecbda7e6e455d44da2bbde
Valentin Funk <funk.valentin@gmail.com>
2016-07-06 08:55:58 +0200

    Integration test behave

--- user_service/tests/integration/config.py
+++ user_service/tests/integration/config.py
@@ -4,6 +4,9 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db')
+if os.environ['MYSQL_HOST']:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://' + os.environ['MYSQL_USER'] + ':' + os.environ['MYSQL_PASSWORD'] + '@' + os.environ['MYSQL_HOST'] + '/' + os.environ['MYSQL_DATABASE']
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 JWT_SECRET_KEY = "test123"
