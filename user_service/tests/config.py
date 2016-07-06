9518aa51b82db5b1cf421aa096b7cc3027b324d2
Valentin Funk <funk.valentin@gmail.com>
2016-07-06 08:53:03 +0200

    Allow integration tests against mysql

--- user_service/tests/config.py
+++ user_service/tests/config.py
@@ -5,5 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = False
 TESTING = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db')
+if os.environ['MYSQL_HOST']:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://' + os.environ['MYSQL_USER'] + ':' + os.environ['MYSQL_PASSWORD'] + '@' + os.environ['MYSQL_HOST'] + '/' + os.environ['MYSQL_DATABASE']
+else:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db')
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
